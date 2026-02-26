bc087d8630f84295835c713291582e7c91d63536
Ferley León <ferlellws@gmail.com>
2026-02-26 12:53:54 -0500

    chore: corregir URLs de frontend y regenerar Gemfile.lock

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:4000/api/v1',
-    wsUrl: 'ws://localhost:4000/cable'
+    apiUrl: 'http://localhost:3000/api/v1',
+    wsUrl: 'ws://localhost:3000/cable'
 };
